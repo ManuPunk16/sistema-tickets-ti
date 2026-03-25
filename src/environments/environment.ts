4b0fde78945d565ae73694e1e6c9b6cf64c1b4df
Luis Hernandez <luisadrianhdzsoto@gmail.com>
2026-03-25 15:42:57 -0600

    feat: actualizar configuración de entorno con credenciales de Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,14 @@
 export const environment = {
   production: false,
+  apiUrl: 'https://tickets-ti-cj.vercel.app/api',
+  urlBase: 'https://tickets-ti-cj.vercel.app',
   firebase: {
-    apiKey: "TU_API_KEY",
-    authDomain: "tu-proyecto.firebaseapp.com",
-    projectId: "tu-proyecto",
-    storageBucket: "tu-proyecto.appspot.com",
-    messagingSenderId: "TU_MESSAGING_ID",
-    appId: "TU_APP_ID"
+    apiKey: "AIzaSyB74tehlcjON5Q39S1QpE1DQoIwtVcrZmc",
+    authDomain: "sistema-tickets-ti-cj.firebaseapp.com",
+    projectId: "sistema-tickets-ti-cj",
+    storageBucket: "sistema-tickets-ti-cj.firebasestorage.app",
+    messagingSenderId: "463512806740",
+    appId: "1:463512806740:web:2c702f3bfd7635009d14c1",
+    measurementId: "G-YXT17SM465"
   }
 };
